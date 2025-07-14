fcd9eddd0d7364627d7dd90e4d36e223c9b9bc8c
范宇轩 <fyx112376@qq.com>
2025-07-14 20:02:23 +0800

    1

---
+++ app.vue
@@ -0,0 +1,35 @@
+<!-- 安装pnpm add -D less less-loader     安装pnpm install mitt-->
+<script setup lang="ts">
+import GoodsSale from './src/components/GoodsSale.vue';
+import MenuBanner from './src/components/MenuBanner.vue';
+import SiteFooter from './src/components/SiteFooter.vue';
+import SiteInfo from './src/components/SiteInfo.vue';
+import SubChannel from './src/components/SubChannel.vue';
+import ToolBar from './src/components/ToolBar.vue';
+import TopHeader from './src/components/TopHeader.vue';
+import LocationWindow from './src/parts/LocationWindow.vue';
+import VideoWindow from './src/parts/VideoWindow.vue';
+import WeChatWindow from './src/parts/WeChatWindow.vue';
+
+</script>
+
+<template>
+    <div class="index-page">
+    <TopHeader />
+    <MenuBanner />
+    <SubChannel />
+    <div class="gray-page">
+        <!-- <FlashSale />这是秒杀板块 -->
+        <GoodsSale />
+    </div>
+    <SiteFooter />
+    <div class="gray-page">
+        <SiteInfo />
+    </div>
+    <ToolBar />
+    <!-- 弹窗 -->
+    <!-- <VideoWindow /> -->
+    <!-- <WeChatWindow /> -->
+    <!-- <LocationWindow /> -->
+    </div>
+</template>
---
+++ src/bus.js
@@ -0,0 +1,5 @@
+//需要安装pnpm install mitt
+import mitt from 'mitt';
+
+const bus = mitt();
+export default bus;
